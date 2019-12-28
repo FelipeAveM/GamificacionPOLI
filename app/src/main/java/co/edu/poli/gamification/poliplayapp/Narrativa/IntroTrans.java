3433ebc2249b6bc68757d7125fef2eb572100650
PipeAm <mfavendano@poligran.edu.co>
2019-12-27 19:21:54 -0500

    PoliPlayApp Version 2.1

---
+++ app/src/main/java/co/edu/poli/gamification/poliplayapp/Narrativa/IntroTrans.java
@@ -0,0 +1,24 @@
+package co.edu.poli.gamification.poliplayapp.Narrativa;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import co.edu.poli.gamification.poliplayapp.R;
+import co.edu.poli.gamification.poliplayapp.Secuencia.SeleccionarTransporte;
+
+public class IntroTrans extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_trans);
+
+    }
+
+    public void toTrans(View view){
+        Intent i = new Intent(this, SeleccionarTransporte.class);
+        startActivity(i);
+    }
+}
